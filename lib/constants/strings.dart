23bd30fb6ae32046611588265a00d35e87b62545
Lasitha Prabodha <lasiprabo@gmail.com>
2021-08-22 17:03:33 +0530

    first commit

---
+++ lib/constants/strings.dart
@@ -0,0 +1,94 @@
+import 'package:mywebsite/models/education.dart';
+
+class Strings {
+  Strings._();
+
+  // home:----------------------------------------------------------------------
+  static const String portfolio = 'Portfolio';
+  static const String this_web = 'Yes this website uses f***ing ';
+  static const String cookies = 'cookies';
+  static const String in_flutter = 'in flutter';
+  static const String heart = ' ❤ ';
+  static const String made_with = 'Made with';
+
+  // intro:---------------------------------------------------------------------
+  static const String hello = 'Hello';
+  static const String name = 'Lasitha Prabodha Weligampola';
+  static const String i_am_lasitha = '\nI am\nLasitha';
+  static const String handle = 'LU6_\nFER';
+  static const String mobile_dev =
+      'Web/Mobile App Developer\nbased in Colombo\nSri Lanka.';
+
+  // about:---------------------------------------------------------------------
+  static const String about_me = 'About Me';
+  static const String about = 'Ab_\nout';
+  static const String me = ' Me';
+  static const String headline =
+      'I\'am Lasitha Prabodha, Web/Mobile App Developer from Colombo, Sri Lanka';
+  static const String summary =
+      'Detailed-oriented, responsible and committed software engineer, with a get it done, on time and high quality product spirit. 4+ years of experience defining requirements, designing, implementing and delivering complex web applications and mobile apps both iOS and Android using variety of programming languages and technologies.';
+  static const String work_summary =
+      'I have proven industrial experience for the 3 main frontend frameworks in current time; Angular, ReactJS and VueJS. \n\nAlso experienced in Vanilla JS, Redux, RxJs, React Native, PHP, Flutter, NodeJS, Ngnix, ASP.NET Core 3.1, HTML5, CSS/SASS and MySQL. I\'m knowledgeable about Spring Boot, Python and many other JavaScript frameworks and libraries. \n\nI worked mainly under hospitality and travel domains.';
+  // skills:--------------------------------------------------------------------
+  static const String skills = 'Sk_\nills';
+  static const String experience = 'Experience';
+  static const String skills_i_have = 'What Skills I Have';
+  static const String rights_reserved =
+      '© 2021 LASITHA PRABODHA WELIGAMPOLA. ALL RIGHTS RESERVED.';
+
+  static final skillsList = [
+    'JavaScript',
+    'Dart',
+    'Flutter',
+    'Android',
+    'Reactive Programming',
+    'Angular',
+    'ReactJS',
+    'VueJS',
+    'PHP',
+    'C#',
+  ];
+
+  static final educationList = [
+    Education(
+      'Feb 2019',
+      'Present',
+      'Creative Software',
+      'Sr. Software Engineer',
+    ),
+    Education(
+      'Mar 2018',
+      'Jan 2019',
+      'Villvay Systems',
+      'Software Engineer',
+    ),
+    Education(
+      'Nov 2016',
+      'Mar 2018',
+      'Findmyfare.com',
+      'Software Engineer',
+    ),
+  ];
+
+  // hire:----------------------------------------------------------------------
+  static const String hire = 'HIRE';
+  static const String always_interested =
+      'I\'m always interested about cool stuff. Are you minding a project?\n';
+  static const String lets_talk = 'Let\'s talk.';
+
+  // menu items:----------------------------------------------------------------
+  static const String menu_facebook = 'FACEBOOK';
+  static const String menu_twitter = 'TWITTER';
+  static const String menu_linked_in = 'LINKEDIN';
+  static const String menu_medium = ' MEDIUM ';
+  static const String menu_github = 'GITHUB';
+  static const String menu_mail = 'MAIL';
+
+  // links:---------------------------------------------------------------------
+  static const String menu_facebook_link = 'https://www.facebook.com/lasithe';
+  static const String menu_twitter_link = 'https://twitter.com/luziferKolla';
+  static const String menu_linked_in_link =
+      'https://www.linkedin.com/in/lasithaprabodha/';
+  static const String menu_github_link = 'https://github.com/LasithaPrabodha';
+  static const String menu_mail_link = 'mailto:lasiprabo@gmail.com';
+}
